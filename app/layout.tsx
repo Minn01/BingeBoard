b7bf0f61c25c511165dba5bded90048e10bda88a
Minn01 <min12200514@gmail.com>
2026-01-11 22:36:58 +0700

    deployment fix vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import Header from "./components/Header"
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutClient from "./components/LayoutClient";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
@@ -27,9 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${robotoMono.variable} antialiased`}
       >
-        {/* the header is in the LayoutClient component */}
         <LayoutClient>
           {children}
         </LayoutClient>
