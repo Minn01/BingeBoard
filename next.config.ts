9262449315ebcb2258c46f2cacb244226d187de3
Minn01 <min12200514@gmail.com>
2026-01-11 22:24:43 +0700

    fix for deployment vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH ,
-  typescript: {
-    ignoreBuildErrors: true, // Skip TypeScript errors
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Skip ESLint errors
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
